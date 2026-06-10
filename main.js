c117fa36e1781112ab55e8f44baa9af463128bbe
Wesley Luciano <wesleyluciano0406@gmail.com>
2026-06-10 20:13:03 -0300

    Criação de uma função para realizar a chamada da API.

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 const URL = 'https://6a29eb54f59cb8f65f1dcaff.mockapi.io/Materiais'
+
+async function chamarAPI() {
+    const resposta = await fetch(URL);
+    if( resposta.status == 200) {
+        const objeto = await resposta.json();
+        console.log(objeto);
+    }
+}
+
+chamarAPI()

0e14a409c830820dd642ecbe2fdc992194f5c445
Wesley Luciano <wesleyluciano0406@gmail.com>
2026-06-10 21:27:09 -0300

    adição do método POST para inserção de dados na lista.

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 const URL = 'https://6a29eb54f59cb8f65f1dcaff.mockapi.io/Materiais'
 
 const tbody = document.querySelector('#tbody-materiais');
-
+const form = document.querySelector('form');
 
 async function chamarAPI() {
     const resposta = await fetch(URL);
     if(resposta.status == 200) {
         const materiais = await resposta.json();
+
+        tbody.innerHTML = '';
        
         materiais.forEach(material => {
             const tr = document.createElement('tr');
@@ -19,4 +21,32 @@ async function chamarAPI() {
   }
 }
 
-chamarAPI()
+async function cadastrarMaterial(nome, quantidade) {
+    const resposta = await fetch(URL, {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+            inputNome: nome,
+            inputQuantidade: quantidade
+        })
+    });
+
+    if (resposta.status == 201) {
+        chamarAPI();
+    }
+}
+
+form.addEventListener('submit', (evento) => {
+    evento.preventDefault();
+
+    const nome = document.querySelector('#input-nome').value;
+    const quantidade = document.querySelector('#input-quantidade').value;
+
+    if (nome && quantidade) {
+        cadastrarMaterial(nome, quantidade);
+        form.reset();
+    }
+});
+
+
+chamarAPI();

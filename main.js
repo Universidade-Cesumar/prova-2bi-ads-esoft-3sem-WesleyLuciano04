595af9cda5f1bf64c7f549b4e2d4ab45b0d57646
Wesley Luciano <wesleyluciano0406@gmail.com>
2026-06-10 21:15:38 -0300

    implementação de função para o consumo de API e preenchimento da lista.

--- main.js
+++ main.js
@@ -1,11 +1,22 @@
 const URL = 'https://6a29eb54f59cb8f65f1dcaff.mockapi.io/Materiais'
 
+const tbody = document.querySelector('#tbody-materiais');
+
+
 async function chamarAPI() {
     const resposta = await fetch(URL);
-    if( resposta.status == 200) {
-        const objeto = await resposta.json();
-        console.log(objeto);
-    }
+    if(resposta.status == 200) {
+        const materiais = await resposta.json();
+       
+        materiais.forEach(material => {
+            const tr = document.createElement('tr');
+            tr.innerHTML = `
+                <td>${material.inputNome}</td>
+                <td>${material.inputQuantidade}</td>
+            `;
+        tbody.appendChild(tr);
+        })
+  }
 }
 
 chamarAPI()

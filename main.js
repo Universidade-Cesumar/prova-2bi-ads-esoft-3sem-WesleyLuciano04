acc38f4d5fc6ac99ca216bea56f5e1cbc695bb20
Wesley Luciano <wesleyluciano0406@gmail.com>
2026-06-17 20:31:32 -0300

    Alteração estrutural para estilização do projeto.

--- main.js
+++ main.js
@@ -15,11 +15,12 @@ async function chamarAPI() {
             tr.innerHTML = `
                 <td>${material.inputNome}</td>
                 <td>${material.inputQuantidade}</td>
-                <label for = "input-retirada"> Retirar Material </label>
-                <input type="text" id = "input-retirada" placeholder = "Ex: 10"> </input>
-                <input type="submit" value = "Retirar"></input>
-                <input type="submit" value = "Excluir"></input>
-
+                <td class=edicao-estoque>
+                    <label for = "input-retirada"> Quantidade </label>
+                    <input type="text" id = "input-retirada" placeholder = "Ex: 10"> </input>
+                    <input type="submit" class="btn-baixar" value = "Baixar"></input>
+                    <input type="submit" class="btn-excluir" value = "Excluir"></input>
+                </td>
             `;
         tbody.appendChild(tr);
         })
